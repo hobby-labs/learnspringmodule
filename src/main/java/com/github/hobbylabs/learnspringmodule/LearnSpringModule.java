310ea6f910c740157001a7c7602bcf6eb7c9aae7
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-06 13:30:50 +0900

    Updated

---
+++ src/main/java/com/github/hobbylabs/learnspringmodule/LearnSpringModule.java
@@ -0,0 +1,10 @@
+package com.github.hobbylabs.learnspringmodule;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearnSpringModule {
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
