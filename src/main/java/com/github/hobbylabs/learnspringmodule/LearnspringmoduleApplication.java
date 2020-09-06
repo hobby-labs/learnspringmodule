4d691577af9b26ba16a0e72c15c9b0078da0881f
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-06 13:01:33 +0900

    Initial commit

---
+++ src/main/java/com/github/hobbylabs/learnspringmodule/LearnspringmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.learnspringmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hobbylabs/learnspringmodule/LearnspringmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.learnspringmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
