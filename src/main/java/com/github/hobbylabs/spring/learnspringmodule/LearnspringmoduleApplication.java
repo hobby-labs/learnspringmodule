0211da94e2161cc10e8a7fdff39a6e4488d4d20e
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-09-07 21:49:32 +0900

    Re-initialize project

---
+++ src/main/java/com/github/hobbylabs/spring/learnspringmodule/LearnspringmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.github.hobbylabs.spring.learnspringmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringmoduleApplication.class, args);
+	}
+
+}
--- src/test/java/com/github/hobbylabs/spring/learnspringmodule/LearnspringmoduleApplicationTests.java
+++ src/test/java/com/github/hobbylabs/spring/learnspringmodule/LearnspringmoduleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.hobbylabs.learnspringmodule;
+package com.github.hobbylabs.spring.learnspringmodule;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
